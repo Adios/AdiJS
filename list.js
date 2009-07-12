fb0e073a355819359911f58011f1d62a9df12883
Adios <adiosf6f@gmail.com>
2009-07-12 13:39:19 +0800

    trailing white spaces and adding eol.

--- list.js
+++ list.js
@@ -2,7 +2,7 @@
 	var	window = this,
 		_List = window.List,
 		undefiend,
-		
+
 		List = window.List = function( elem ) {
 			return new List.fn.init( elem );
 		};
@@ -10,26 +10,26 @@
 	List.fn = List.prototype = {
 
 		init: function( elem ) {
-			
+
 			this.entries = new Array;
-			
+
 			for ( var i = 0, ns = elem.childNodes, nsl = ns.length; i < nsl; i++ ) {
 				if ( ns[i].nodeType == 1 ) this.entries.push( ns[i] );
-			}			
-			 
+			}
+
 			this.root = elem;
-			
+
 			return this;
 		},
-		
+
 		size: function() {
 			return this.entries.length;
 		},
-		
+
 		/* insert( index, DOM, ...) or insert( DOM, ... ) with index = 0 */
 		insert: function() {
 			var i = 0, index = 0, length = this.size();
-			
+
 			if ( typeof arguments[0] == 'number' ) {
 				index = parseInt( arguments[0] );
 				if ( index > length || index < 0 ) index = 0;
@@ -38,21 +38,21 @@
 
 			/* accepted a list of dom, e.g. insert(0, dom, dom, dom) */
 			var frag = document.createDocumentFragment();
-			
+
 			for ( var al = arguments.length; i < al; i++ ) {
 				frag.appendChild( arguments[i] );
 			}
-			
+
 			/* attach to the DOM tree */
 			this.root.insertBefore( frag, length ? this.entries[index] : null  );
-			
+
 			return this.init( this.root );
 		},
-		
+
 		/* remove( index, index, index, ... ) */
 		remove: function() {
-			var index,	
-			
+			var index,
+
 				/* unique the arguments, return an array of elements to be removed. */
 				queue = (function( entries, length ) {
 					var a = new Array;
@@ -61,24 +61,24 @@
 						for ( var j = i - 1; j >= 0; j-- ) {
 							if ( this[j] === this[i] ) j = --i;
 						}
-						
+
 						/* resolution, push to array */
 						var index = parseInt( this[i] );
 						if ( index >= 0 && index <= length - 1 ) a.push( entries[index] );
 					}
 					return a;
 				}).apply( Array.prototype.slice.call( arguments ), [this.entries, this.size()] ),
-				
+
 				ql = queue.length - 1;
-				
+
 			for ( ; ql >= 0; ql-- ) { this.root.removeChild( queue[ql] ); }
-			
+
 			return this.init( this.root );
 		}
 	};
 
 	List.fn.init.prototype = List.prototype;
-	
+
 	List.noConflict = function() {
 		window.List = _List;
 		return List;
--- test/test-list.js
+++ test/test-list.js
@@ -4,9 +4,9 @@ $(function() {
 		for ( var i = 0, al = arguments.length; i < al; i++ ) {
 			if ( $.trim($('ul.todo > *').eq(i).text()) != arguments[i] ) return false;
 		}
-		return true;		
+		return true;
 	};
-	
+
 	function createEntry( text ) {
 		var e = document.createElement( 'li' );
 		e.appendChild( document.createTextNode( text ) );
@@ -15,68 +15,68 @@ $(function() {
 
 
 	module( 'List' );
-	
+
 	test( 'List.noConflict()', function() {
 		ok( window.List = (function() {
-			var L = window.List, 
+			var L = window.List,
 				l = List.noConflict();
 			return ( l === L && window.List === undefined ) ? l : false;
-		})(), 'it works' );		
+		})(), 'it works' );
 	} );
-	
+
 	test( 'over empty list', function() {
 		var list = List( $('ul.empty').get(0) );
-		
+
 		ok( list.reorder(), "reorder shoudn't cause errors" );
-		
+
 		difference( 'List( $("ul.empty").get(0) ).size()', 0, function() {
 			ok( list.remove( 0 ), "remove one shouldn't cause errors" );
 		} );
-		
+
 		difference( 'List( $("ul.empty").get(0) ).size()', 1, function() {
 			ok( list.insert( createEntry( 'new one') ), "insert one shouldn't cause errors" );
-		} );		
+		} );
 	} );
 
-	test( 'insert()', function() {		
+	test( 'insert()', function() {
 		var list = List( $('ul.todo').get(0) );
 
-		difference( 'List( $("ul.todo").get(0) ).size()', function() { 
+		difference( 'List( $("ul.todo").get(0) ).size()', function() {
 			ok( list.insert( createEntry( '1' ) ) , 'insert( e )' );
 		} );
-		
-		difference( 'List( $("ul.todo").get(0) ).size()', 3, function() { 
-			ok( 
-				list.insert( createEntry( '2' ), createEntry( '3' ), createEntry( '4' ) ) , 
-				'insert( e, e, e )' 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', 3, function() {
+			ok(
+				list.insert( createEntry( '2' ), createEntry( '3' ), createEntry( '4' ) ) ,
+				'insert( e, e, e )'
 			);
 		} );
 		ok ( isInPosition( '2', '3', '4', '1' ), 'they should be in position' );
-		
-		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() { 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() {
 			ok( list.insert( 1, createEntry( '5' ) ) , 'insert( 1, e )' );
 		} );
 		ok ( isInPosition( '2', '5', '3' ), 'they sholud be in position' );
-		
-		difference( 'List( $("ul.todo").get(0) ).size()', 3, function() { 
-			ok( 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', 3, function() {
+			ok(
 				list.insert( 2, createEntry( '6' ), createEntry( '7' ), createEntry( '8' ) ),
 				'insert( 2, e, e, e )'
 			);
 		} );
 		ok ( isInPosition( '2', '5', '6', '7', '8', '3' ), 'they should be in position' );
-	
-		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() { 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() {
 			ok( list.insert( -1, createEntry( '9' ) ), 'insert( -1, e )' );
 		} );
 		ok ( isInPosition( '9', '2'), 'it should be at the head');
-		
-		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() { 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() {
 			ok( list.insert( 200, createEntry( '10' ) ), 'insert( 200, e )' );
 		} );
-		ok ( isInPosition( '10', '9'), 'it should be at the head'); 
+		ok ( isInPosition( '10', '9'), 'it should be at the head');
 	} );
-	
+
 	test( 'remove()', function() {
 		var list = List( $('ul.todo').get(0) );
 
@@ -85,41 +85,41 @@ $(function() {
 		} );
 
 		ok ( isInPosition( 'a', 'b', 'c', 'e', 'f', 'g' ), 'they should be in position' );
-		
+
 		difference( 'List( $("ul.todo").get(0) ).size()', -3, function() {
 			ok( list.remove( 0, 3, 2 ), 'remove( 0, 3, 2 )' );
 		} );
 		ok ( isInPosition( 'b', 'f', 'g' ), 'they should be in position' );
-		
+
 		difference( 'List( $("ul.todo").get(0) ).size()', -2, function() {
 			ok( list.remove( 1, 1, 1, 2 ), 'remove( 1, 1, 1, 2 )' );
 		} );
 		ok ( isInPosition( 'b' ), 'they should be in position' );
-		
+
 		difference( 'List( $("ul.todo").get(0) ).size()', 0, function() {
 			ok( list.remove( 100, -100, undefined ), 'remove( 100, -100, undefined )' );
 		} );
-		ok ( isInPosition( 'b' ), 'they should be in position' );		
+		ok ( isInPosition( 'b' ), 'they should be in position' );
 	} );
 
 	test( 'reorder()', function() {
 		var list = List( $('ul.todo').get(0) );
-		
+
 		ok ( list.reorder( {reverse: true} ), 'sort reversely' );
 		ok ( isInPosition( 'g', 'f', 'e', 'd', 'c', 'b', 'a' ), 'they should be in position' );
-		
+
 		ok ( list.reorder(), 'sort' );
 		ok ( isInPosition( 'a', 'b', 'c', 'd', 'e', 'f', 'g' ), 'they should be in position' );
-		
+
 		ok ( list.reorder( {shuffle: true} ), 'shuffle' );
-		ok ( isInPosition( 'a', 'b', 'c', 'd', 'e', 'f', 'g' ), "their positions shouldn't be the same position as berfore" );		
-	
+		ok ( isInPosition( 'a', 'b', 'c', 'd', 'e', 'f', 'g' ), "their positions shouldn't be the same position as berfore" );
+
 		ok ( list.reorder( {fn: priority} ), 'sort by priority' );
 		ok ( isInPosition( 'd', 'g', 'e', 'a', 'f', 'b', 'c' ), 'they should be in position' );
-		
+
 		ok ( list.reorder( {fn: priority, reverse: true} ), 'sort in reverse by priority' );
 		ok ( isInPosition( 'c', 'b', 'f', 'a', 'e', 'g', 'd' ), 'they should be in position' );
-		
+
 		ok ( list.reorder( {eyecandy: true} ), 'sort with eyecandy enabled)' );
 		ok ( isInPosition( 'a', 'b', 'c', 'd', 'e', 'f', 'g' ), 'they should be in position' );
 	} );
@@ -127,46 +127,46 @@ $(function() {
 	test( 'chain', function() {
 		var list = List( $('ul.todo').get(0) );
 
-		difference( 'List( $("ul.todo").get(0) ).size()', 0, function() { 
+		difference( 'List( $("ul.todo").get(0) ).size()', 0, function() {
 			ok( list.insert( createEntry( 'aa' ) ).remove( 0 ) , 'insert( e ).remove( 0 )' );
 		} );
-		
-		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() { 
-			ok( 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', 1, function() {
+			ok(
 				list.insert( createEntry( 'bb' ) ).insert( createEntry( 'cc' ) )
 					.remove( 0 ).insert( createEntry( 'dd' ), createEntry( 'ee' ) )
 					.remove( 1, 2 )
-				, 'insert( e ).insert( e ).remove( 0 ).insert( e, e ).remove( 1, 2 )' 
+				, 'insert( e ).insert( e ).remove( 0 ).insert( e, e ).remove( 1, 2 )'
 			);
 		} );
 		ok ( isInPosition( 'dd' ), 'they should be in position' );
-			
-		difference( 'List( $("ul.todo").get(0) ).size()', 0, function() { 
-			ok( 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', 0, function() {
+			ok(
 				list.insert( 1, createEntry( 'ff' ) ).insert( 0, createEntry( 'gg' ) )
 					.remove( 0 ).insert( 1, createEntry( 'hh' ), createEntry( 'ii' ) )
 					.remove( 1, 2, 3 )
-				, 'insert( 1, e ).insert( 0, e ).remove( 0 ).insert( 1, e, e ).remove( 1, 2, 3 )' 
+				, 'insert( 1, e ).insert( 0, e ).remove( 0 ).insert( 1, e, e ).remove( 1, 2, 3 )'
 			);
 		} );
 		ok ( isInPosition( 'dd' ), 'they should be in position' );
-		
-		difference( 'List( $("ul.todo").get(0) ).size()', -7, function() { 
-			ok( 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', -7, function() {
+			ok(
 				list.insert( -100, createEntry( 'jj' ) ).insert( 100, createEntry( 'kk' ) )
 					.remove( 0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 1, 8, 9, 10 ).insert( createEntry( 'mm' ) )
-				, 'insert( -100, e ).insert( 100, e ).remove( a lot ).insert( e )' 
+				, 'insert( -100, e ).insert( 100, e ).remove( a lot ).insert( e )'
 			);
 		} );
 		ok ( isInPosition( 'mm' ), 'they should be in position' );
-		
-		difference( 'List( $("ul.todo").get(0) ).size()', -7, function() { 
-			ok( 
+
+		difference( 'List( $("ul.todo").get(0) ).size()', -7, function() {
+			ok(
 				list.insert( createEntry( 'nn' ) ).insert( 1, createEntry( 'oo' ) )
 					.reorder().remove( 1 ).insert( createEntry( 'pp' ) )
-				, 'insert( e ).insert( 1, e ).reorder().remove( 1 ).insert( e )' 
+				, 'insert( e ).insert( 1, e ).reorder().remove( 1 ).insert( e )'
 			);
 		} );
-		ok ( isInPosition( 'pp', 'mm', 'oo' ), 'they should be in position' );	
+		ok ( isInPosition( 'pp', 'mm', 'oo' ), 'they should be in position' );
 	} );
 })
